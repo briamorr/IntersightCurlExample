7d263dec5fcc3e5f8b7e4a03f4f709938a86ccb4
briamorr <78086763+briamorr@users.noreply.github.com>
2022-02-07 10:06:21 -0500

    Update intersightCurlGet.sh

--- intersightCurlGet.sh
+++ intersightCurlGet.sh
@@ -4,9 +4,9 @@
 if [ "$#" -lt 4 ]; then
     echo -e "Intersight CURL Example\n"
     echo -e "Usage: ./script.sh 'API Key ID' 'API Secretkey Filename' 'HTTP method [GET/POST/DELETE]' 'API Endpoint' 'Payload [Required only for POST]\n"
-    echo -e "Example GET: ./inter.sh '1111/2222/3333' 'SecretKey.txt' 'GET' '/api/v1/compute/PhysicalSummaries?\$top=1&\$select=Serial'\n"
-    echo -e "Example DELETE: ./inter.sh '1111/2222/3333' 'SecretKey.txt' 'DELETE' '/api/v1/vnic/LanConnectivityPolicies/60bfd0964fa6a1d629e66eb6'\n"
-    echo -e "Example POST: ./inter.sh '1111/2222/3333' 'SecretKey.txt' 'POST' '/api/v1/ntp/Policies' '{"Organization":{"ObjectType":"organization.Organization","Moid":"5ddea34a6972652d3353b462"},"Name":"myNtpPolicy","Enabled":true,"NtpServers":["1.1.1.1"]}'"
+    echo -e "Example GET: ./script.sh '1111/2222/3333' 'SecretKey.txt' 'GET' '/api/v1/compute/PhysicalSummaries?\$top=1&\$select=Serial'\n"
+    echo -e "Example DELETE: ./script.sh '1111/2222/3333' 'SecretKey.txt' 'DELETE' '/api/v1/vnic/LanConnectivityPolicies/60bfd0964fa6a1d629e66eb6'\n"
+    echo -e "Example POST: ./script.sh '1111/2222/3333' 'SecretKey.txt' 'POST' '/api/v1/ntp/Policies' '{"Organization":{"ObjectType":"organization.Organization","Moid":"5ddea34a6972652d3353b462"},"Name":"myNtpPolicy","Enabled":true,"NtpServers":["1.1.1.1"]}'"
     exit 2
 fi
 
