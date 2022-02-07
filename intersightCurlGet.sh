2ba026693a7d74b0069414128802bd68262155f5
briamorr <78086763+briamorr@users.noreply.github.com>
2022-02-07 09:36:21 -0500

    Update intersightCurlGet.sh

--- intersightCurlGet.sh
+++ intersightCurlGet.sh
@@ -1,24 +1,50 @@
 #!/bin/bash
-#Intersight CURL GET example
+#Intersight CURL example
 
-#Instructions:
-#Substitute apiKey value with APIkey from Intersight
-#Substitute SecretKey.txt file location with API secret key file from Intersight
+if [ "$#" -lt 4 ]; then
+    echo -e "Intersight CURL Example\n"
+    echo -e "Usage: ./script.sh 'API Key ID' 'API Secretkey Filename' 'HTTP method [GET/POST/DELETE]' 'API Endpoint' 'Payload [Required only for POST]\n"
+    echo -e "Example GET: ./inter.sh '1111/2222/3333' 'SecretKey.txt' 'GET' '/api/v1/compute/PhysicalSummaries?\$top=1&\$select=Serial'\n"
+    echo -e "Example DELETE: ./inter.sh '1111/2222/3333' 'SecretKey.txt' 'DELETE' '/api/v1/vnic/LanConnectivityPolicies/60bfd0964fa6a1d629e66eb6'\n"
+    echo -e "Example POST: ./inter.sh '1111/2222/3333' 'SecretKey.txt' 'POST' '/api/v1/ntp/Policies' '{"Organization":{"ObjectType":"organization.Organization","Moid":"5ddea34a6972652d3353b462"},"Name":"myNtpPolicy","Enabled":true,"NtpServers":["1.1.1.1"]}'"
+    exit 2
+fi
 
-#Details:
-#apibodyDigest is SHA-256 digest of the payload, in the case of a GET request null
-#apiEndpoint is the endpoint URL in lower case, must be in lower case...
-#apiTime is RFC1123 time
-#apiSignaure is the headers in a sha256 digest signed by the secretkey file and converted to base 64
+hostName="intersight.com"
+apiKey=$1
+apiSecretKey=$2
+method=${3^^}
+url=$4
+payload=$5
 
-apibodyDigest="digest: SHA-256="$(printf '' | openssl dgst -sha256 -binary | base64)
-apiKey='xxx/yyy/zzz'
-apiEndpoint='/api/v1/compute/physicalsummaries?$top=1&$select=serial'
 apiTime="date: "$(date -u '+%a, %d %b %Y %T %Z')
-apiSignature=$(
-printf "(request-target): get %s
-%s
-%s
-host: intersight.com" "$apiEndpoint" "$apiTime" "$apibodyDigest" | openssl dgst -sha256 -binary -sign SecretKey.txt | base64 -w 0)
 
-curl 'https://www.intersight.com/api/v1/compute/PhysicalSummaries?$top=1&$select=Serial' -H 'Accept: "application/json"' -H 'Host: intersight.com' -H "$apiTime" -H "$apibodyDigest" -H 'Authorization: Signature keyId="'"$apiKey"'",algorithm="rsa-sha256",headers="(request-target) date digest host",signature="'"$apiSignature"'"'
+#SHA256 digest of HTTP payload
+apibodyDigest="digest: SHA-256="$(printf ''$payload'' | openssl dgst -sha256 -binary | base64)
+
+#SHA256 digest of headers signed by private key file, header info must be in lower case for the digest
+apiSignature=$(
+printf "(request-target): %s %s
+%s
+%s
+host: %s" "${method,,}" "${url,,}" "$apiTime" "$apibodyDigest" "$hostName" | openssl dgst -sha256 -binary -sign $2 | base64 -w 0)
+
+
+if [ $method = "POST" ]
+then
+curl -X $method "https://"$hostName$url \
+-H 'Accept: "application/json"' \
+-H 'Content-Type: application/json' \
+-H 'Host: '$hostName -H "$apiTime" -H "$apibodyDigest" \
+-H 'Authorization: Signature keyId="'"$apiKey"'",algorithm="rsa-sha256",headers="(request-target) date digest host",signature="'"$apiSignature"'"' \
+-d $payload
+elif [ $method = "GET" ] || [ $method = "DELETE" ]
+then
+curl -X $method "https://"$hostName$url \
+-H 'Accept: "application/json"' \
+-H 'Host: '$hostName -H "$apiTime" -H "$apibodyDigest" \
+-H 'Authorization: Signature keyId="'"$apiKey"'",algorithm="rsa-sha256",headers="(request-target) date digest host",signature="'"$apiSignature"'"'
+fi
+
+
+
